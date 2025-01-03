7c0757456580c7f4e0d037c1155e057a52f2e899
Ibrahim <ibrahimalkhalilridene.prepa@gmail.com>
2025-01-03 02:32:25 +0100

    CHANGES

--- main.py
+++ main.py
@@ -1,11 +1,44 @@
-from pipeline import run_Duffing_problem_anomaly_mesure
+from pipeline import (run_Duffing_problem_anomaly_mesure,
+                      run_Symbolization_comparison_for_WS,
+                      run_Markov_order_comparison_for_WS)
 
 
 
 
 if __name__ == "__main__":
     
-    run_Duffing_problem_anomaly_mesure(
+    #run_Duffing_problem_anomaly_mesure(
+    #    beta_min=0.10,          # min value of beta
+    #    beta_max=0.35,          # max value of beta
+    #    num_beta=20,            # number of beta values to test between beta_min and beta_max
+    #    nominal_beta=0.10,      # reference value of beta to consider as normal data
+    #    A=22.0,                 # Driving amplitude of Duffing problem
+    #    omega=5.0,              # Driving frequency of Duffing problem
+    #    sampling_rate=100,      # sampling rate of the data
+    #    total_time=40,          # Total duration of the time series in seconds
+    #    models=['PCA', 'MLPNN', 'RBFNN', 'SFNN', 'WS'],  # List of models to run: ['PCA', 'MLPNN', 'RBFNN', 'SFNN', 'WS']
+    #    alphabet_size=8,        # size of alphabet for symbolization
+    #    D=1,                    # order of the Markov Chain
+    #    threshold=0.95,         # cumulative variance threshold for selecting principal components
+    #    symbolization='SAX'     # type of symbolization (linear or SAX)
+    #)
+    
+    
+    #run_Symbolization_comparison_for_WS(
+    #    beta_min=0.10,          # min value of beta
+    #    beta_max=0.35,          # max value of beta
+    #    num_beta=20,            # number of beta values to test between beta_min and beta_max
+    #    nominal_beta=0.10,      # reference value of beta to consider as normal data
+    #    A=22.0,                 # Driving amplitude of Duffing problem
+    #    omega=5.0,              # Driving frequency of Duffing problem
+    #    sampling_rate=100,      # sampling rate of the data
+    #    total_time=40,          # Total duration of the time series in seconds
+    #   alphabet_size=8,        # size of alphabet for symbolization
+    #    D=1,                    # order of the Markov Chain
+    #)
+    
+    
+    run_Markov_order_comparison_for_WS(
         beta_min=0.10,          # min value of beta
         beta_max=0.35,          # max value of beta
         num_beta=20,            # number of beta values to test between beta_min and beta_max
@@ -14,9 +47,7 @@ if __name__ == "__main__":
         omega=5.0,              # Driving frequency of Duffing problem
         sampling_rate=100,      # sampling rate of the data
         total_time=40,          # Total duration of the time series in seconds
-        models=['PCA', 'MLPNN', 'RBFNN', 'SFNN', 'WS'],  # List of models to run: ['PCA', 'MLPNN', 'RBFNN', 'SFNN', 'WS']
         alphabet_size=8,        # size of alphabet for symbolization
-        D=1,                    # order of the Markov Chain
-        threshold=0.95          # cumulative variance threshold for selecting principal components
+        D_values=[1,2,3,4,5],       # The list of orders of the Markov Chain
+        symbolization='SAX'     # type of symbolization (linear or SAX)
     )
-    
---
+++ plot/plots.py
@@ -0,0 +1,79 @@
+import matplotlib.pyplot as plt
+
+
+
+def plot_models(anomaly_mesures):
+    """
+    Plots the evolution of the anomaly measures over beta values for different models.
+    Args:
+        anomaly_mesures(dict): Dictionary containing the anomaly measures for each model.
+    """
+    
+    markers = ['o', 'x', 's', 'd', '^']
+    
+    # Plot all anomaly measures
+    plt.figure(figsize=(12, 8))
+    
+    for i, (name, mesure) in enumerate(anomaly_mesures.items()):
+        plt.plot(list(mesure.keys()), list(mesure.values()), marker=markers[i], label=name)
+    
+    plt.xlabel('Beta values (β)')
+    plt.ylabel('Normalized Anomaly Measure')
+    plt.title('Comparison of Anomaly Detection Methods (Normalized)')
+    plt.grid()
+    plt.legend()
+    plt.savefig('plot/anomaly_comparison_plot.png', dpi=300, bbox_inches='tight')
+    
+    
+    
+    
+    
+    
+def plot_symbolization_comparison_WS(ws_anomaly_measures_sax, ws_anomaly_measures_linear):
+    """
+    Plots the comparison between the linear and the SAX symbolization for WS method.
+    Args:
+        ws_anomaly_measures_sax(dict): Dictionary containing the anomaly measures for different beta values for SAX symbolization.
+        ws_anomaly_measures_linear(dict): Dictionary containing the anomaly measures for different beta values for linear symbolization.
+    """
+    
+    # Plot all anomaly measures
+    plt.figure(figsize=(12, 8))
+    
+    plt.plot(list(ws_anomaly_measures_sax.keys()), list(ws_anomaly_measures_sax.values()), marker='o', label='SAX')
+    plt.plot(list(ws_anomaly_measures_linear.keys()), list(ws_anomaly_measures_linear.values()), marker='x', label='Linear')
+    
+    plt.xlabel('Beta values (β)')
+    plt.ylabel('Normalized Anomaly Measure')
+    plt.title('Comparison of Anomaly Detection Methods for WS for different Symbolization techniques')
+    plt.grid()
+    plt.legend()
+    plt.savefig('plot/Symbolization_comparison_plot.png', dpi=300, bbox_inches='tight')
+    
+    
+    
+    
+    
+    
+    
+    
+def plot_Markov_order_comparison_for_WS(anomaly_mesures):
+    """
+    Plots the evolution of the anomaly measures over beta values for different Markov orders D for WS method on Duffing problem.
+    Args:
+        anomaly_mesures(dict): Dictionary containing the anomaly measures for each Markov order.
+    """
+    
+    
+    # Plot all anomaly measures
+    plt.figure(figsize=(12, 8))
+    
+    for (order, mesure) in anomaly_mesures.items():
+        plt.plot(list(mesure.keys()), list(mesure.values()), label=f'D={order}')
+    
+    plt.xlabel('Beta values (β)')
+    plt.ylabel('Normalized Anomaly Measure')
+    plt.title('Comparison of Anomaly Detection Methods for WS for different Markov orders D')
+    plt.grid()
+    plt.legend()
+    plt.savefig('plot/Markov_orders_comparison_plot.png', dpi=300, bbox_inches='tight')
